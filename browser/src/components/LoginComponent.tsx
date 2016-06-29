7b65bf2f63098d5214c9876117e3067023c6707f
Chris Kitching <chriskitching@linux.com>
2016-06-29 06:08:06 +0100

    Indicate when the username/password are wrong

--- browser/src/components/LoginComponent.tsx
+++ browser/src/components/LoginComponent.tsx
@@ -2,21 +2,28 @@ import 'jquery';
 import 'bootstrap';
 import {ipcRenderer} from 'electron';
 import * as MessageTypes from "../../../node/src/net/MessageTypes";
+import * as classNames from 'classnames';
 const style: any = require('../styles/login.scss');
 
 import * as React from "react";
+import Timer = NodeJS.Timer;
 
 interface LoginProps {}
 
 interface LoginState {
     username?: string;
     password?: string;
+
+    angry?: boolean;
 }
 
 /**
  * The login/registration screen. Runs in its own window,
  */
 class LoginComponent extends React.Component<LoginProps, LoginState> {
+    // Used to trigger the removal of the redness from the UI.
+    angryTimer: Timer;
+
     constructor(props: LoginProps) {
         super();
 
@@ -28,6 +35,10 @@ class LoginComponent extends React.Component<LoginProps, LoginState> {
         // Listen for when we're told the credentials are wrong.
         ipcRenderer.on('authentication_failed', (event: any, msg: MessageTypes.AuthenticationFailed) => {
             console.error("Incorrect login");
+            this.setState({angry: true});
+            this.angryTimer = setTimeout(() => {
+                this.setState({angry: false});
+            }, 1000);
         });
     }
 
@@ -42,6 +53,7 @@ class LoginComponent extends React.Component<LoginProps, LoginState> {
 
     handleSubmit(e:any) {
         e.preventDefault();
+        clearTimeout(this.angryTimer);
 
         // Send the crap to the server.
         ipcRenderer.send('login', this.state.username, this.state.password);
@@ -49,6 +61,8 @@ class LoginComponent extends React.Component<LoginProps, LoginState> {
     }
 
     render() {
+        let classnames:string = classNames("form-control", {"angry-form-control": this.state.angry});
+
         return (
 
 <div className="container loginScreen">
@@ -58,11 +72,11 @@ class LoginComponent extends React.Component<LoginProps, LoginState> {
             <form role="form" onSubmit={(e:any) => this.handleSubmit(e)}>
                 <div className="form-group">
                     <label>Username:</label>
-                    <input type="text" className="form-control" value={this.state.username} onChange={(e:any) => this.handleUsernameChange(e)}/>
+                    <input type="text" className={classnames} value={this.state.username} onChange={(e:any) => this.handleUsernameChange(e)}/>
                 </div>
                 <div className="form-group">
                     <label>Password:</label>
-                    <input type="password" className="form-control" value={this.state.password} onChange={(e:any) => this.handlePasswordChange(e)}/>
+                    <input type="password" className={classnames} value={this.state.password} onChange={(e:any) => this.handlePasswordChange(e)}/>
                 </div>
                 <div className="checkbox">
                     <label><input type="checkbox"/> Remember me</label>
